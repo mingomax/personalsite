1b078a36b6b31be3e44b0986247dbd377e90c0a1
Domingos Teruel <dteruel@its.jnj.com>
2022-03-21 23:22:03 -0300

    Added badge circleci on README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # My personal website
 
+[![CircleCI](https://circleci.com/gh/mingomax/personalsite/tree/circleci-project-setup.svg?style=svg)](https://circleci.com/gh/mingomax/personalsite/tree/circleci-project-setup)
+
 ### Overview
 
 Starter personal site template boilerplate based on Webpack with SASS/PostCSS and babel ES6/7 support
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const compression = require("compression");
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
 const chalk = require("chalk");
+const basicAuth = require("express-basic-auth");
 
 const app = express();
 const teal500 = chalk.hex("#009688");
@@ -12,14 +12,20 @@ const user = process.env.BASIC_USER;
 const pass = process.env.BASIC_PASS;
 
 if (user && pass) {
-  app.use(express.basicAuth(user, pass));
+  app.use(
+    basicAuth({
+      users: { dteruel: "p4ssw4rd$" },
+      challenge: true,
+      realm: "DT Website",
+    })
+  );
 }
 
 // app.use(express.logger("dev"));
 // enable compress
-// app.use(compression());
-// // get our app to use body parser
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(compression());
+// get our app to use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("dist"));
 
