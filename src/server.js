36cb99c9bb92a27677f11394d2a663d57f555768
Domingos Teruel <mingomax@dteruel.net.br>
2022-03-22 19:50:48 -0300

    Integrated with Circleci and Deployment on Heroku (#2)
    Co-authored-by: Domingos Teruel <dteruel@its.jnj.com>

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const compression = require("compression");
+const express = require("express");
+const bodyParser = require("body-parser");
+const chalk = require("chalk");
+const basicAuth = require("express-basic-auth");
+
+const app = express();
+const teal500 = chalk.hex("#009688");
+const HOST = process.env.HOST || "0.0.0.0";
+const PORT = process.env.PORT || 3000;
+const user = process.env.BASIC_USER;
+const pass = process.env.BASIC_PASS;
+
+if (user && pass) {
+  app.use(
+    basicAuth({
+      users: { dteruel: "p4ssw4rd$" },
+      challenge: true,
+      realm: "DT Website",
+    })
+  );
+}
+
+// app.use(express.logger("dev"));
+// enable compress
+app.use(compression());
+// get our app to use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("dist"));
+
+app.listen(PORT, HOST, () => {
+  console.log(teal500("🚀  App: Bootstrap Succeeded"));
+  console.log(teal500(`🚀  Host: http://${HOST}:${PORT}`));
+  console.log(teal500(`"Static dir: ${__dirname}`));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
