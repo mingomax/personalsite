2f1bfa7977562c206957fa9c952fa24eed9abb0d
Domingos Teruel <dteruel@its.jnj.com>
2022-03-27 01:02:31 -0300

    Stylized menu navigation (desktop and mobile)

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -2,6 +2,6 @@ import * as menu from "./components/menu";
 import * as header from "./components/header";
 
 // initiaize header
-// header.initMainHeader();
+header.initMainHeader();
 // Initialize mainMenu
 menu.initToggleMenu();
